f106eda1984f8fcd64f3e186965030b287d58695
M.Fatih <mehmetfatihaltuntas58@gmail.com>
2026-07-18 16:51:17 +0300

    Ilk proje kodlari ve Java calismalari eklendi

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Konsola Metin Yazdırma
+        System.out.println("HelloWorld");
+
+        // Variable oluşturma
+        int stuNo = 1013;
+        // int = data türü   stuNo = Variable ismi   1013 = Variable değeri
+
+        String name = "Fatih";
+        boolean student = true;
+
+        // Olusturdugumuz degiskenleri ekrana yazdiralim
+        System.out.println("Öğrenci ismi : "+ name );
+        System.out.println("Öğrenci Numarası : "+ stuNo);
+        System.out.println("Öğrenci Kayıtlı mı : "+student);
+
+
+    }
+}
